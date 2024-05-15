e8eb72f8762e880ba531088ec62e8c52c1bec24a
hgatayaandf <121156667+hgatayaandf@users.noreply.github.com>
2024-05-15 13:12:08 +0530

    Update index.js

--- index.js
+++ index.js
@@ -91,7 +91,7 @@ app.get('/', (req, res) => {
 });
 
 // Start the server on port 3000
-const port = 3000;
+const port = 2000;
 app.listen(port, () => {
     console.log(`Server is running on http://localhost:${port}`);
 });
